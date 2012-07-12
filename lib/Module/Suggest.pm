b02a03c682e15600de235d25308d837525fef55f
tokuhirom <tokuhirom@gmail.com>
2012-07-12 09:43:23 +0900

    implemented

--- lib/Module/Suggest.pm
+++ lib/Module/Suggest.pm
@@ -3,8 +3,25 @@ use strict;
 use warnings;
 use 5.008008;
 our $VERSION = '0.01';
+use Carp;
 
-
+BEGIN {
+    our %SUGGEST_MAP = (
+        'JSON::Any' => 'There is no reason to use JSON::Any today. use JSON.pm instead.',
+        'base' => 'use parent.pm',
+        'Unicode::Japanese' => 'use Encode and Lingua::JA::Regular::Unicode',
+        'HTTP::MobileAttribute' => "It's deprecated. use HTTP::MobileAgent instead.",
+    );
+    unshift @INC, sub {
+        my ($code, $name) = @_;
+        $name =~ s!/!::!g;
+        $name =~ s/\.pm$//;
+        if (my $message = $SUGGEST_MAP{$name}) {
+            carp $message;
+        }
+        return 0;
+    }
+}
 
 1;
 __END__
@@ -13,15 +30,15 @@ __END__
 
 =head1 NAME
 
-Module::Suggest - A module for you
+Module::Suggest - A suggestion for you
 
 =head1 SYNOPSIS
 
-  use Module::Suggest;
+    perl -MModule::Suggest -e 'use JSON::Any'
 
 =head1 DESCRIPTION
 
-Module::Suggest is
+Module::Suggest suggests better module, i seem.
 
 =head1 AUTHOR
 
